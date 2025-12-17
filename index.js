64ab85a8254b6bd3c8125e68a04b05c087bd5fef
Mohit Kumar <mohit.sircl@gmail.com>
2025-12-17 23:47:49 +0530

    change port in index

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
 
-app.listen(PORT, () => {
+app.listen(PORT||3000, () => {
     console.log(`Server is running on port ${PORT}`);
 })
