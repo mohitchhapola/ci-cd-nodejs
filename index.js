bd0b335ac05d36f215be56b7b36173b516326af4
Mohit Kumar <86540460+mohitchhapola@users.noreply.github.com>
2026-04-27 10:31:44 +0530

    Fix PORT assignment to use process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env || 4000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
